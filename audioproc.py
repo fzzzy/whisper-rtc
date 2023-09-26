853b16f119f2f4b50bef1c75107cdce2029373ed
Donovan Preston <donovanpreston@gmail.com>
2023-09-25 22:59:12 -0400

    initial commit

---
+++ audioproc.py
@@ -0,0 +1,43 @@
+
+
+import numpy as np
+from pydub import AudioSegment
+
+RMSS = np.array([])
+
+
+def detect_silence(
+    audio_data,
+    sample_rate=48000,
+    window_length=0.5,
+    threshold=5e-3
+):
+    silence_points = []
+    window_size = int(window_length * sample_rate)
+    # 0.5 sec * 48k = 24k samples
+    n_windows = len(audio_data) // window_size
+        
+    # Normalize
+    audio_data = audio_data / 32767.0
+
+    for i in range(n_windows):
+        start = i * window_size
+        end = (i + 1) * window_size
+
+        rms = np.sqrt(np.mean(np.square(audio_data[start:end])))
+
+        #print(rms, threshold)
+
+        if rms < threshold:
+            return (start, end)
+
+
+def save_as_mp3(samples, sample_rate, filename):
+    audio = AudioSegment(
+        samples.tobytes(),
+        frame_rate=sample_rate,
+        sample_width=2,
+        channels=1
+    )
+    audio.export(f"{filename}.mp3", format="mp3")
+
---
+++ main.py
@@ -0,0 +1,96 @@
+
+
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
+from aiortc.mediastreams import MediaStreamError
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+import numpy
+from pydantic import BaseModel
+
+import audioproc
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Offer(BaseModel):
+    sdp: str
+    type: str
+
+
+@app.post("/offer/")
+async def handle_offer(offer: Offer):
+    pc = RTCPeerConnection()
+    offer_sdp = RTCSessionDescription(sdp=offer.sdp, type=offer.type)
+
+    @pc.on("datachannel")
+    def on_datachannel(channel):
+        print("Debug: Datachannel event triggered")
+        # Handle data channel events here
+
+    @pc.on("iceconnectionstatechange")
+    def on_iceconnectionstatechange():
+        print("ICE Connection State is:", pc.iceConnectionState)
+
+    @pc.on("track")
+    async def on_track(track):
+        if isinstance(track, MediaStreamTrack):
+            samples = None
+            i = 0
+            min_data_length = int(1 * 48000)  # 1 second for instance
+            while True:
+                try:
+                    frame = await track.recv()
+                    if samples is None:
+                        print("\n\n\n\n\nFRAME\n\n\n\n\nFRAME\n\n\n\n\n\nFRAME")
+                except MediaStreamError:
+                    print("disconnected", len(samples))
+                    audioproc.save_as_mp3(
+                        samples, frame.sample_rate, i)
+                    return
+
+                # Accumulate samples
+                ndar = frame.to_ndarray()[0][::2]
+                if samples is None:
+                    samples = ndar
+                else:
+                    samples = numpy.concatenate((samples, ndar))
+
+                # Only start detecting silence after accumulating enough data
+                if len(samples) < min_data_length:
+                    continue
+
+                # Run silence detection
+                while True:
+                    detected = audioproc.detect_silence(samples)
+                    if detected is None:
+                        break
+                    (start_sample, end_sample) = detected
+                    if not start_sample:
+                        samples = samples[end_sample:]
+                        continue
+                    print("saving segment", i, start_sample)
+                    audioproc.save_as_mp3(
+                        samples[:start_sample], frame.sample_rate, i)
+                    i += 1
+                    samples = samples[end_sample:]
+
+
+    await pc.setRemoteDescription(offer_sdp)
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+
+
+@app.get("/", response_class=HTMLResponse)
+def index():
+    return open('index.html').read()
